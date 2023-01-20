d88300b99fa8f9531bd41452d61ddb2df1b3947f
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2023-01-20 11:05:19 +0530

    small update

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> 
-      <div>
+      <header className="App-header">
+        <div className='navbar'><a href='https://web5lab.net'><img src='media/logo.png' className='navbar_web5lab_logo'></img></a><h1>Powered By Web5Lab</h1></div> 
+      <div className='app_content'>
         <h1> Project Underdevlopment</h1>
       </div>      
       </header>
