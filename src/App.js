b5ed2dd6e7756c8eac71716a88ef4af0996d9c30
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2023-02-10 13:00:28 +0530

    added number counter

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ const customStyles = {
 };
 function App() {
   const [modal, setmodal] = useState(false)
+  const [tempCounter, settempCounter] = useState(4)
   const [TxtCounter, setTxtCounter] = useState('')
+  const [rows, setRows] = useState('1\n2\n3\n4\n');
   const [txtValue, setTxtValue] = useState('')
   const OpenModal = () => {
     setmodal(true)
@@ -37,9 +39,23 @@ function App() {
     // var Str = JSON.stringify(data_01);
     return finalStr;
   }
-
-  const lineCounter = () => {
-    
+   
+  
+  const lineCounter = (event) => {
+    let temp = event.target.value.split("\n").length 
+    settempCounter(temp);
+    setTxtValue(event.target.value);
+    if (tempCounter == temp ) {
+      console.log('same number')
+    } else {
+      var t = '';
+      var y = 0
+      for (let i = 0; i < temp; i++) {
+         y = y + 1
+         t = t + y + '\n'
+      }
+      setRows(t)
+    }
   }
 
   useEffect(() => {
@@ -74,8 +90,8 @@ function App() {
       <header className="App-header">
         <div className='navbar'><div className='navbar_left'><a href='https://web5lab.net'><img src='media/logo.png' className='navbar_web5lab_logo'></img></a><h1>Powerd By Web5lab</h1></div><div className='navbar_right'><a className='testnet_btn' href='https://cryptotool.in'>Mainet Version</a><button className='Connect_Btn btn' onClick={ConnectWallet}>Connect Wallet</button></div></div>
         <div className='app_content'>
-          <textarea rows={1} cols={1} className="input_box_sub"></textarea>
-          <textarea rows={20} className="input_box" value={txtValue} onChange={(e) => { setTxtValue(e.target.value) }} cols={40}></textarea>
+          <textarea rows={1} cols={1} value={rows} className="input_box_sub"></textarea>
+          <textarea rows={20} className="input_box" value={txtValue} onChange={(e) => { lineCounter(e) }} cols={40}></textarea>
         </div>
         <div>
           <button className='btn' onClick={OpenModal}>import csv file</button>
